b8828b2dad124048a8572bf4333f15ed70fbe8dd
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2023-02-03 09:43:32 -0800

    all user routes functioning as anticipated

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,7 @@ module.exports = {
      },
      // Get a single user
      async getSingleUser(req, res) {
-        const user = await User.findOne({ _id: req.params.userId })
+        const user = await User.findOne({ _id: req.params.userId }).populate("friends")
         if(!user) {
             res.status(404).json({ message: "No user found with that ID" })
         } res.status(200).json(user)
